d0a41d00f2346afbf929b65afdeeb87ae6404f20
Tegar Prasetya <57427115+gantengtegar@users.noreply.github.com>
2020-07-13 15:25:53 +0700

    Add files via upload

---
+++ UAS/database/tb_mhs.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 13, 2020 at 10:07 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uas_oop1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_mhs`
+--
+
+CREATE TABLE `tb_mhs` (
+  `nim` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jurusan` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tb_mhs`
+--
+
+INSERT INTO `tb_mhs` (`nim`, `nama`, `jurusan`, `alamat`) VALUES
+('18090037', 'Solehudin Alfah Rezi', 'DIV Teknik Informatika', 'Pagedangan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_mhs`
+--
+ALTER TABLE `tb_mhs`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
